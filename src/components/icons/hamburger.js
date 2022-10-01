7661eb66c8dbff81f18336a05c00e26bcf83a31d
ianmhenriquez <ianmhenriquez@hotmail.com>
2022-10-01 13:57:25 -0400

    added react-spring and fixed mobile nav

---
+++ src/components/icons/hamburger.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from '../base';
+import { device } from '../base/devices';
+
+const StyledContainer = styled(Container)`
+    min-width: 40px;
+    width: 40px;
+    max-width: 40px;
+    height: 40px;
+    min-height: 40px;
+    max-height: 40px;
+    color: ${({ isOpen }) => isOpen ? '#2589bd' : 'grey'};
+    user-select: none;
+
+    @media ${device.laptopL} {
+        display:none;
+    }
+
+    @media ${device.mobileL}{
+        display:block;
+    }
+    
+`;
+
+const SVG = styled.svg`
+    width: 100%;
+    height: 100%;
+    color:${props => props.theme.colors[props.iconColor]};
+`;
+
+const HamburgerIcon = ({ className, id, isOpen, iconColor="defaultSecondary" }) => (
+    <StyledContainer className={className} id={id} isOpen={isOpen}>
+        <SVG iconColor={iconColor} role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></SVG>
+    </StyledContainer>
+);
+
+export default HamburgerIcon;
--- src/components/sub/NavBar.js
+++ src/components/sub/NavBar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from 'styled-components'
 import { Container, Row } from "../base";
 import { NavLink } from "react-router-dom";
+import { device } from "../base/devices";
+import HamburgerIcon from "../icons/hamburger";
+import { useSpring, animated } from "react-spring";
 
 const StyledNav = styled(Container).attrs({
     as: "nav"
@@ -12,26 +15,84 @@ const StyledNav = styled(Container).attrs({
    padding-bottom:${props => props.theme.spacing.doubleInset};
 `
 
-const LinkItem = styled.div`
+const LinkItemBurger = styled.div`
     display:flex;
-    width: ${props => props.width ? [props.width] : '100%'};
+    width:25%;
     justify-content:space-between;
+
+    @media ${device.mobileL} { 
+        display:none;
+    }
 
 `;
 
+const LinkItem = styled.div`
+    display:flex;
+    width:75%;
+    justify-content:space-between;
+
+
+`;
+
+
+
+const StyledRow = styled(Row)`
+    @media ${device.mobileL} { 
+        flex-direction:row;
+        justify-content:space-around;
+    }
+`;
+
+const iconContainer = styled.div`
+`
+const AnimatedIcon = styled(animated(iconContainer))`
+`;
+
+const Item = styled(Container)`
+    flex-direction:column;
+    align-items:center;
+    height:100%;
+    justify-content:space-evenly;
+`;
+
+
 const Navbar = () => {
+    const [isOpened, setIsOpened] = useState(false);
+    const {
+        size,
+        visible,
+        ...springProps
+    } = useSpring({
+        size: isOpened ? 400 : 0,
+        visible: isOpened ? 'block' : 'none',
+        from: {
+            size: 0,
+            visible: 'none'
+        }
+    });
+
     return (
         <StyledNav>
-            <Row justifyContent="end">
-                <LinkItem width="75%">
-                    <NavLink style={{textDecoration: 'none', color:'white'}} to="/">Temp home</NavLink>
+            <StyledRow justifyContent="end">
+                <LinkItem>
+                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/">Temp home</NavLink>
                 </LinkItem>
-                <LinkItem width="25%">
-                    <NavLink style={{textDecoration: 'none', color:'white'}} to="/projects">Projects</NavLink>
-                    <NavLink style={{textDecoration: 'none', color:'white'}} to="/downloads">Downloads</NavLink>
-                    <NavLink style={{textDecoration: 'none', color:'white'}} to="/contact">Contact</NavLink>
-                </LinkItem>
-            </Row>
+                <LinkItemBurger width="25%">
+                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/projects">Projects</NavLink>
+                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/downloads">Downloads</NavLink>
+                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/contact">Contact</NavLink>
+                </LinkItemBurger>
+                <div onClick={() => setIsOpened(e => !e)}>
+                    <HamburgerIcon />
+                </div>
+                <AnimatedIcon style={{ height: size, width: size, display: visible, ...springProps }} >
+                    <Item >
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/projects">Projects</NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/downloads">Downloads</NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/contact">Contact</NavLink>
+                    </Item>
+                </AnimatedIcon>
+            </StyledRow>
         </StyledNav>
     );
 }
