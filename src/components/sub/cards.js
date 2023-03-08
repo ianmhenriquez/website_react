8e789523ee64f269571808a1c86a7babe3312d18
ianmhenriquez <ianmhenriquez@hotmail.com>
2023-03-08 13:03:51 -0500

    made cards more mapable

--- src/components/sub/cards.js
+++ src/components/sub/cards.js
@@ -4,6 +4,7 @@ import { Container, Row, Column } from "../base";
 import { device } from "../base/devices";
 import { useSpring, animated, useChain, useTransition, useSpringRef, config } from "react-spring";
 import Tilt from 'react-parallax-tilt';
+import { useInView } from 'react-intersection-observer';
 
 const AnimatedTile = styled(animated(Container))`
     background-color:pink;
@@ -22,7 +23,6 @@ const Card = styled(animated(Container))`
     width:100%;
     align-items:center;
     padding-bottom:30px;
-
 `
 
 const AnimatedH1 = styled(animated.h1)`
@@ -38,68 +38,24 @@ const StyledTilt = styled(Tilt)`
 
 
 const Cards = () => {
-    const fromLeft = useSpring({
-        from: { opacity: 0, marginLeft: -100, marginRight: 100 },
-        to: { opacity: 1, marginLeft: 0, marginRight: 0 }
-    });
-
-    const fromRight = useSpring({
-        from: { opacity: 0, marginLeft: 100, marginRight: -100 },
-        to: { opacity: 1, marginLeft: 0, marginRight: 0 }
-    });
+    //make animation happen when entering scroll position
+    //to do this we need to use a ref to the element we want to animate and a ref to the animation itself 
+    const {ref, inView} = useInView()
 
     const fromTop = useSpring({
         from: { opacity: 0, marginBottom: 100, marginTop: -100 },
-        to: { opacity: 1, marginBottom: 0, marginTop: 20 }
+        to: { opacity: inView ? 1 : 0, marginBottom: inView ? 0 : 100, marginTop: inView ? 20 : -100 }
     });
 
     return (
-        <Container justifyContent="center" >
+        <Container justifyContent="center" ref={ref} >
             <StyledRow>
-                <StyledTilt>
-                    <Card direction="column" on>
-                        <AnimatedH1 style={fromLeft}>
-                            Title placeHolder
-                        </AnimatedH1>
-                        <AnimatedTile style={fromLeft}>
-                            <Row direction="direction">
-                                <Column>
-                                    <p>picture placeholder</p>
-                                </Column>
-                                <Column>
-                                    <p>
-                                        text
-                                    </p>
-                                </Column>
-                            </Row>
-                        </AnimatedTile>
-                    </Card>
-                </StyledTilt>
                 <StyledTilt>
                     <Card direction="column" width="45%">
                         <AnimatedH1 style={fromTop}>
                             Title placeHolder
                         </AnimatedH1>
                         <AnimatedTile style={fromTop}>
-                            <Row direction="direction">
-                                <Column>
-                                    <p>picture placeholder</p>
-                                </Column>
-                                <Column>
-                                    <p>
-                                        text
-                                    </p>
-                                </Column>
-                            </Row>
-                        </AnimatedTile>
-                    </Card>
-                </StyledTilt>
-                <StyledTilt>
-                    <Card direction="column">
-                        <AnimatedH1 style={fromRight}>
-                            Title placeHolder
-                        </AnimatedH1>
-                        <AnimatedTile style={fromRight}>
                             <Row direction="direction">
                                 <Column>
                                     <p>picture placeholder</p>
