d4f0d49ead51f399cc20e457b3e90ae3d6a7efe0
ianmhenriquez <ianmhenriquez@hotmail.com>
2023-03-08 18:19:44 -0500

    made cards fully mapped and changed color

--- src/components/sub/cards.js
+++ src/components/sub/cards.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from 'styled-components'
 import { Container, Row, Column } from "../base";
 import { device } from "../base/devices";
 import { useSpring, animated, useChain, useTransition, useSpringRef, config } from "react-spring";
 import Tilt from 'react-parallax-tilt';
 import { useInView } from 'react-intersection-observer';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const AnimatedTile = styled(animated(Container))`
-    background-color:pink;
+    background-color:#E6E6E6;
     width:100%;
 `;
 
@@ -33,42 +35,67 @@ const StyledTilt = styled(Tilt)`
     @media ${device.mobileL}{
         width:75%;
     }
+    cursor:pointer;
+`;
+
+const Image = styled.img`
+    width:100%;
+    height:100%;
+    object-fit:cover;
 `;
 
 
 
 const Cards = () => {
-    //make animation happen when entering scroll position
-    //to do this we need to use a ref to the element we want to animate and a ref to the animation itself 
-    const {ref, inView} = useInView()
-
+    const { ref, inView } = useInView()
+    const navigate = useNavigate();
+    const HandleClick = () => {
+        navigate('/projects');
+    }
     const fromTop = useSpring({
         from: { opacity: 0, marginBottom: 100, marginTop: -100 },
         to: { opacity: inView ? 1 : 0, marginBottom: inView ? 0 : 100, marginTop: inView ? 20 : -100 }
     });
+    //get data from api
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                'https://bored-beret-hen.cyclic.app/api/projects',
+            );
+            setData(result.data);
+        };
+        fetchData();
+    }, []);
+
+
 
     return (
         <Container justifyContent="center" ref={ref} >
             <StyledRow>
-                <StyledTilt>
-                    <Card direction="column" width="45%">
-                        <AnimatedH1 style={fromTop}>
-                            Title placeHolder
-                        </AnimatedH1>
-                        <AnimatedTile style={fromTop}>
-                            <Row direction="direction">
-                                <Column>
-                                    <p>picture placeholder</p>
-                                </Column>
-                                <Column>
-                                    <p>
-                                        text
-                                    </p>
-                                </Column>
-                            </Row>
-                        </AnimatedTile>
-                    </Card>
-                </StyledTilt>
+                {data.map((item, index) => (
+                    <StyledTilt key={item._id}>
+                        <div onClick={HandleClick}>
+                            <Card direction="column" width="45%">
+                                <AnimatedH1 style={fromTop}>
+                                    {item.title}
+                                </AnimatedH1>
+                                <AnimatedTile style={fromTop}>
+                                    <Row direction="direction">
+                                        <Column>
+                                            <Image src={item.image} />
+                                        </Column>
+                                        <Column>
+                                            <p>
+                                                {item.shortDescription}
+                                            </p>
+                                        </Column>
+                                    </Row>
+                                </AnimatedTile>
+                            </Card>
+                        </div>
+                    </StyledTilt>
+                ))}
             </StyledRow>
         </Container>
     )
